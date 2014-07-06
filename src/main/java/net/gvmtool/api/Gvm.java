102de707cf9e6d17bb7b8ef83cb97a17ae619938
noamt <noam@10ne.org>
2014-07-06 15:33:17 +0300

    Remove redundant GVM main methods

--- src/main/java/net/gvmtool/api/Gvm.java
+++ src/main/java/net/gvmtool/api/Gvm.java
@@ -6,38 +6,15 @@ package net.gvmtool.api;
 public class Gvm {
 
     public static Object use(String candidateName) {
-        return constructUse(candidateName);
-    }
-
-    public static Object getUse(String candidateName) {
-        return use(candidateName);
+        return new Use(new Object(), candidateName);
     }
 
     public static Object install(String candidateName) {
-        return constructInstall(candidateName);
-    }
-
-    public static Object getInstall(String candidateName) {
-        return install(candidateName);
+        return new Object();
     }
 
     public static Object uninstall(String candidateName) {
-        return constructUninstall(candidateName);
-    }
-
-    public static Object getUninstall(String candidateName) {
-        return uninstall(candidateName);
-    }
-
-    private static Object constructUse(String candidateName) {
         return new Object();
     }
 
-    private static Object constructInstall(String candidateName) {
-        return new Object();
-    }
-
-    private static Object constructUninstall(String candidateName) {
-        return new Object();
-    }
 }
