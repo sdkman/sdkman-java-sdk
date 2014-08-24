2a7be328bc1df3e6d9a540fdd3d7e32d4e0e0c76
noamt <noam@10ne.org>
2014-08-24 16:01:35 +0300

    Update the GVMSpec for the new Java API

--- src/test/groovy/net/gvmtool/api/GvmSpec.groovy
+++ src/test/groovy/net/gvmtool/api/GvmSpec.groovy
@@ -13,16 +13,16 @@ class GvmSpec extends Specification {
 
     def 'Use'() {
         expect:
-        Gvm.use.grails(version: '2.1.4') == Paths.get(System.properties['user.home'], '.gvm', 'grails', '2.1.4')
+        Gvm.use('grails').version('2.1.4') == Paths.get(System.properties['user.home'], '.gvm', 'grails', '2.1.4')
     }
 
     def 'Install'() {
         expect:
-        Gvm.install.grails(version: '2.1.4') == Paths.get(System.properties['user.home'], '.gvm', 'grails', '2.1.4')
+        Gvm.install('grails').version('2.1.4') == Paths.get(System.properties['user.home'], '.gvm', 'grails', '2.1.4')
     }
 
     def 'Uninstall'() {
         expect:
-        Gvm.uninstall.grails(version: '2.1.5')
+        Gvm.uninstall('grails').version('2.1.5')
     }
 }
